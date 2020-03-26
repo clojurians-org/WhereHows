416bd7ceb69edf8de99090285617645ae144a434
larluo <larluo@clojurians.org>
2020-03-26 10:31:24 +0800

    add java test

--- contrib/metadata-ingestion/bin/dataset-jdbc-generator.hs
+++ contrib/metadata-ingestion/bin/dataset-jdbc-generator.hs
@@ -1,15 +1,16 @@
 #! /usr/bin/env nix-shell
-#! nix-shell dataset-jdbc-generator.hs.nix -i runghc
+#! nix-shell dataset-jdbc-generator.hs.nix -i "runghc --ghc-arg=-fobject-code"
 
 {-# LANGUAGE OverloadedStrings, FlexibleInstances, FlexibleContexts, ScopedTypeVariables #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
 
+import Control.Concurrent (runInBoundThread)
 import Language.Java (withJVM)
 import Language.Java.Inline
 
 main :: IO ()
-main = withJVM [] [java| {
+main = runInBoundThread $ withJVM [] [java| {
   System.out.println("Hello Java!") ;
 } |]
  
